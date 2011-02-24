e4d3a3f9cff10a11505b67eea514aae5ea149390
Justin Forest <justin.forest@gmail.com>
2011-02-24 14:22:24 +0300

    Web site udpdate.

--- src/macros.py
+++ src/macros.py
@@ -103,15 +103,20 @@ def pagelist(pages, limit=5, label=None, show_dates=True):
     return u'Ничего нет.'
 
 
+def add_comments(page):
+    labels = get_post_labels(page)
+    if page.get('file', '').endswith('.mp3') or 'blog' in labels:
+        settings = 'var disqus_identifier = "'+ page.url +'";'
+        return u'<div id="disqus_thread"></div><script type="text/javascript">if (window.location.href.indexOf("http://localhost:") == 0) var disqus_developer = 1;'+ settings +' (function() { var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true; dsq.src = \'http://tmradio.disqus.com/embed.js\'; (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq); })();</script><noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=tmradio">comments powered by Disqus.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>'
+    return ''
+
 def print_player(page):
     extra = u''
     link = page.get('file', '')
     if link.endswith('.mp3') or page.url.startswith('blog/'):
         extra = u'<p>Слушать выпуск:</p><div id="player"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="165" height="37" id="swf"><param name="movie" value="niftyplayer.swf?file='+ urllib.quote(link) +'"><param name="quality" value="high"/><param name="bgcolor" value="#FFFFFF"/><embed src="niftyplayer.swf?file='+ urllib.quote(link) +'" quality="high" bgcolor="#FFFFFF" width="165" height="37" name="swf" type="application/x-shockwave-flash" swLiveConnect="true" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></div>'
         extra += u'<p><a href="%s">Скачать выпуск</a></p>' % escape(link)
-
-        settings = 'var disqus_identifier = "'+ page.url +'";'
-        extra += u'<div id="disqus_thread"></div><script type="text/javascript">if (window.location.href.indexOf("http://localhost:") == 0) var disqus_developer = 1;'+ settings +' (function() { var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true; dsq.src = \'http://tmradio.disqus.com/embed.js\'; (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq); })();</script><noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=tmradio">comments powered by Disqus.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>'
+    extra += add_comments(page)
     return extra
 
 
